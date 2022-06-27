dbf5ddbabf0c120c207c6feb4509c78d7c883e56
JSegovia <acosta.segoviaa@gmail.com>
2022-06-26 23:49:59 -0500

    product created

--- src/controllers/products.ts
+++ src/controllers/products.ts
@@ -1,4 +1,13 @@
 import { Request, Response } from 'express';
+import moment from 'moment';
+import { db } from '../db';
+import { slugExist, slugify } from '../helpers';
+import {
+  ProductCreated,
+  ProductVariant,
+  ProductsVariants,
+  ProductBody,
+} from '../interfaces';
 
 export const getProducts = async (req: Request, res: Response) => {
   try {
@@ -21,11 +30,93 @@ export const getProduct = async (req: Request, res: Response) => {
 };
 
 export const createProduct = async (req: Request, res: Response) => {
+  const {
+    title,
+    description,
+    discount,
+    created_by,
+    updated_by,
+    image_id,
+    variant_options,
+  }: ProductBody = req.body;
+
+  const date = moment().format();
+  const slug = slugify(title);
+  const newSlug = await slugExist(slug, 'products');
+  let variants: ProductVariant[] = [];
+  let productsVariants: ProductsVariants[] = [];
+
   try {
-    return res.status(200).json({ ok: true, msg: 'Producto creado' });
+    const text: string = `
+    INSERT INTO 
+      products(title, slug, description, discount, is_published, is_active, image_id, created_by, updated_by, created_at, updated_at) 
+    VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11) 
+    RETURNING*
+    `;
+
+    const values = [
+      title,
+      newSlug,
+      description,
+      discount,
+      false,
+      true,
+      image_id,
+      created_by,
+      updated_by,
+      date,
+      date,
+    ];
+
+    const { rows } = await db.query(text, values);
+    const productCreated: ProductCreated = rows[0];
+
+    for (let i = 0; i < variant_options.length; i++) {
+      const text: string = `
+      INSERT INTO 
+        variant_options(price, grams, mililiters, length, width, height, diameter, variant_size_id, variant_color_id) 
+      VALUES($1,$2,$3,$4,$5,$6,$7,$8,$9) 
+      RETURNING*`;
+
+      const values = [
+        variant_options[i].price,
+        variant_options[i].grams,
+        variant_options[i].mililiters,
+        variant_options[i].length,
+        variant_options[i].width,
+        variant_options[i].height,
+        variant_options[i].diameter,
+        variant_options[i].variant_size_id,
+        variant_options[i].variant_color_id,
+      ];
+
+      const { rows } = await db.query(text, values);
+
+      variants.push(rows[0]);
+    }
+
+    for (let i = 0; i < variants.length; i++) {
+      const text: string = `INSERT INTO products_variants(product_id, variant_option_id) VALUES($1,$2) RETURNING*`;
+      const values = [productCreated.id, variants[i].id];
+
+      const { rows } = await db.query(text, values);
+
+      productsVariants.push(rows[0]);
+    }
+
+    return res.status(201).json({
+      ok: true,
+      msg: 'Se ha creado el producto exitosamente.',
+      productCreated,
+      variants,
+    });
   } catch (error) {
     console.log({ error });
-    return res.status(500).json({ ok: false, msg: 'Error', error });
+    return res.status(500).json({
+      ok: false,
+      msg: 'Error en el servidor al momento de crear un producto.',
+      error,
+    });
   }
 };
 
---
+++ src/interfaces/products.ts
@@ -0,0 +1,43 @@
+export interface ProductVariant {
+  id: number;
+  price: number;
+  variant_color_id: number;
+  variant_size_id: number;
+  diameter?: number;
+  grams?: number;
+  height?: number;
+  length?: number;
+  mililiters?: number;
+  width?: number;
+}
+
+export interface ProductCreated {
+  id: number;
+  title: string;
+  slug: string;
+  description: string;
+  discount: number;
+  is_published: boolean;
+  is_active: boolean;
+  image_id: string;
+  created_by: number;
+  updated_by: number;
+  created_at: string;
+  updated_at: string;
+}
+
+export interface ProductsVariants {
+  id: number;
+  product_id: number;
+  variant_option_id: number;
+}
+
+export interface ProductBody {
+  title: string;
+  description: string;
+  discount: number;
+  created_by: number;
+  updated_by: number;
+  image_id: string;
+  variant_options: ProductVariant[];
+}
