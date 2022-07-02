53a9c53f49b5f8a606a81a724691367258d19187
JSegovia <acosta.segoviaa@gmail.com>
2022-07-01 23:20:07 -0500

    add rate

--- src/controllers/ratings.ts
+++ src/controllers/ratings.ts
@@ -1,10 +1,49 @@
 import { Request, Response } from 'express';
+import moment from 'moment';
+import { db } from '../db';
 
-export const rateProduct = (req: Request, res: Response) => {
+export const getRateByProduct = async (req: Request, res: Response) => {
   try {
     return res
+      .status(200)
+      .json({ ok: true, msg: 'Calificación de productos.' });
+  } catch (error) {
+    console.log({ error });
+    return res.status(500).json({
+      ok: false,
+      msg: 'Error en el servidor al momento de obtener calificación de producto.',
+    });
+  }
+};
+
+export const rateProduct = async (req: Request, res: Response) => {
+  const { rating, user_id, product_id } = req.body;
+  const date = moment().format();
+
+  try {
+    const textFav: string =
+      'SELECT * FROM ratings WHERE user_id = $1 AND product_id = $2';
+    const valuesFav = [user_id, product_id];
+
+    const ratingExist = await db.query(textFav, valuesFav);
+
+    if (ratingExist.rows.length > 0) {
+      return res
+        .status(400)
+        .json({ ok: false, msg: 'Ya has calificado este producto.' });
+    }
+
+    const text: string =
+      'INSERT INTO ratings(rating, user_id, product_id, created_at) VALUES($1,$2,$3,$4) RETURNING*';
+    const values = [rating, user_id, product_id, date];
+
+    const { rows } = await db.query(text, values);
+
+    const ratingAdded = rows[0];
+
+    return res
       .status(201)
-      .json({ ok: true, msg: 'Se ha agregado su calificación.' });
+      .json({ ok: true, msg: 'Se ha agregado su calificación.', ratingAdded });
   } catch (error) {
     console.log({ error });
     return res.status(500).json({
--- src/routes/ratings.ts
+++ src/routes/ratings.ts
@@ -1,8 +1,30 @@
 import { Router } from 'express';
-import { rateProduct } from '../controllers';
+import { check } from 'express-validator';
+import { getRateByProduct, rateProduct } from '../controllers';
+import { productExist, userExist } from '../helpers';
+import { validateFields, validateJWT, validateUser } from '../middlewares';
 
 const router = Router();
 
-router.post('/', rateProduct);
+router.get('/:id', [], getRateByProduct);
+
+router.post(
+  '/',
+  [
+    validateJWT,
+    validateUser,
+    check('user_id').custom(userExist),
+    check('product_id').custom(productExist),
+    check(
+      'rating',
+      'La calificación tiene que ser un número entero entre 1 y 5.'
+    ).isInt({
+      min: 1,
+      max: 5,
+    }),
+    validateFields,
+  ],
+  rateProduct
+);
 
 export default router;
