fbf2301f5aa058304fd1d0454377cc75c54dcd6a
JSegovia <acosta.segoviaa@gmail.com>
2022-07-13 17:47:15 -0500

    d

--- src/middlewares/validateRoles.ts
+++ src/middlewares/validateRoles.ts
@@ -17,13 +17,13 @@ export const superAdminRol = (
   if (!req.user) {
     return res
       .status(500)
-      .json({ msg: 'Se quiere verificar el rol sin iniciar sesión' });
+      .json({ msg: 'Se quiere verificar el rol sin iniciar sesión.' });
   }
 
-  const { role_id, first_name } = req.user as ReqUser;
+  const { role_id, first_name } = req.user;
   if (role_id !== 1) {
     return res.status(401).json({
-      msg: `${first_name} no tiene permisos de Super Administrador para realizar esa acción`,
+      msg: `${first_name} no tiene permisos de Super Administrador para realizar esa acción.`,
     });
   }
 
@@ -34,13 +34,13 @@ export const adminRol = (req: Request, res: Response, next: NextFunction) => {
   if (!req.user) {
     return res
       .status(500)
-      .json({ msg: 'Se quiere verificar el rol sin iniciar sesión' });
+      .json({ msg: 'Se quiere verificar el rol sin iniciar sesión.' });
   }
 
   const { role_id, first_name } = req.user;
   if (role_id !== 2) {
     return res.status(401).json({
-      msg: `${first_name} no tiene permisos de Administrador para realizar esa acción`,
+      msg: `${first_name} no tiene permisos de Administrador para realizar esa acción.`,
     });
   }
 
@@ -52,11 +52,11 @@ export const hasRol = (...roles: Role[]) => {
     if (!req.user) {
       return res
         .status(500)
-        .json({ msg: 'Se quiere verificar el rol sin iniciar sesión' });
+        .json({ msg: 'Se quiere verificar el rol sin iniciar sesión.' });
     }
     if (!roles.includes(req.user.role)) {
       return res.status(401).json({
-        msg: `Para realizar esta acción se requiere alguno de estos roles: ${roles}`,
+        msg: `Para realizar esta acción se requiere alguno de estos roles: ${roles}.`,
       });
     }
 
